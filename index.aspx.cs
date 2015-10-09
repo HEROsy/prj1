7966b567912aa74700a1de96759bc7e2d69410a1
HEROsy <1035041137@qq.com>
2015-10-09 12:08:31 +0800

    index页后台数据完成

--- index.aspx.cs
+++ index.aspx.cs
@@ -7,8 +7,12 @@ using System.Web.UI.WebControls;
 
 public partial class index : System.Web.UI.Page
 {
+    public String title = "";
+    public String title1 = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        title = "这是网页标题";
+        title1 = "这是正文标题";
     }
 }
